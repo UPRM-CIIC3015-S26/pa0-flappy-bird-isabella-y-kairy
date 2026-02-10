030fd9886ef4013175a5b07da4e0a58951fc5ddd
diegoavicente <diego.vicente@upr.edu>
2026-02-09 23:16:51 -0400

    TASK 5 DONE

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ pygame.display.set_caption("Flappy Bird")
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
-PLAYER = (255, 0, 0)
+PLAYER = (0, 0, 255)
 
 # Font Size -->
 big_font = pygame.font.SysFont(None, 80)
